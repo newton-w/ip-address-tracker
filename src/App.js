96bd1b23d0e1c47bd81328e9171a7d723ac566cd
newton-w <71337527+newton-w@users.noreply.github.com>
2023-01-27 18:35:47 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react'
+import Tracker from './components/Tracker';
+function App() {
+  return (
+   <>
+   <Tracker />
+   </>
+  
+   
+  );
+}
+
+export default App;
